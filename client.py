f821810f6829fe18a43e458eb4bf645d9985ee93
agsgoudinho <aguinaldogoudinho@gmail.com>
2020-03-27 21:00:54 -0300

    try removido do cliente

--- client.py
+++ client.py
@@ -2,30 +2,27 @@ import socket
 import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 sock.connect(('localhost', 9000))
 
-try:
-    while True:
-        expected_data_size = int(sock.recv(4).decode())
-        received_data = ''
+while True:
+    expected_data_size = int(sock.recv(4).decode())
+    received_data = ''
 
-        while len(received_data) < expected_data_size:
-            received_data += sock.recv(4).decode()
-        print(received_data)
+    while len(received_data) < expected_data_size:
+        received_data += sock.recv(4).decode()
+    print(received_data)
 
-        if received_data.lower() == 'see ya':
-            break
+    if received_data.lower() == 'see ya':
+        break
 
-        time.sleep(2)
+    time.sleep(2)
 
-        mensagem = input("cliente: ").strip()
-        send_data_size = len(mensagem)
-        sock.sendall(str(send_data_size).zfill(4).encode())
-        sock.sendall(mensagem.encode())
+    mensagem = input("cliente: ").strip()
+    send_data_size = len(mensagem)
+    sock.sendall(str(send_data_size).zfill(4).encode())
+    sock.sendall(mensagem.encode())
 
-        if mensagem.lower() == "see ya":
-            break
+    if mensagem.lower() == "see ya":
+        break
 
-finally:
-    sock.close()
+sock.close()
